9ab82f099876fa96cf778242ff431f98ffa3ff14
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2020-04-29 01:05:43 +0530

    Complete BLoC pattern - example 01

---
+++ android/app/src/main/kotlin/com/example/flutterblocpattern/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterblocpattern
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutterblocpattern/counter_bloc.dart';
+import 'package:flutterblocpattern/counter_event.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// ---> Events [BLoC] ---> State
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _bloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: StreamBuilder(
+            stream: _bloc.counter,
+            initialData: 0,
+            builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('You have pushed the button this many times: '),
+                  Text(
+                    '${snapshot.data}',
+                    style: Theme.of(context).textTheme.headline4,
+                  )
+                ],
+              );
+            }),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventSink.add(IncrementEvent()),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _bloc.dispose();
+  }
+}
